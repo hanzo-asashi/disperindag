23548606caf5fc99efc57b2b2352a1296087e87b
Gifa Eriyanto <gifaeriyanto@ymail.com>
2015-08-31 21:26:03 +0800

    Tambah Berita

--- protected/modules/admin/views/layouts/main.php
+++ protected/modules/admin/views/layouts/main.php
@@ -33,6 +33,29 @@
 </div>
 <!-- ./page container -->
 
+<!-- Modal 1 (Basic)-->
+<div class='modal fade' id='modal-kategori'>
+    <div class='modal-dialog'>
+        <div class='modal-content'>
+            <form action=''>
+                <div class='modal-header'>
+                    <button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
+                    <h4 class='modal-title'>Tambah Kategori</h4>
+                </div>
+
+                <div class='modal-body'>
+                    <input type='text' class='form-control' placeholder='Tulis Kategori Baru disini'>
+                </div>
+
+                <div class='modal-footer no-margin'>
+                    <button type='button' class='btn btn-default' data-dismiss='modal'>Kembali</button>
+                    <button type='button' class='btn btn-success'>Tambah</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 <!-- page scripts -->
 <script type="text/javascript" src="<?php echo yii::app()->request->baseUrl ?>/static/js/plugins/jquery/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo yii::app()->request->baseUrl ?>/static/js/plugins/bootstrap/bootstrap.min.js"></script>
